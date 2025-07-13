8aac429c13538a35d13c9f9d82b31daf2bb2a574
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-07-13 10:42:37 +0800

    client: init shadcn-ui

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { RouterProvider } from "react-router"
 
+import "./index.css"
 import routes from "./routes"
 
 createRoot(document.getElementById("root") as HTMLElement).render(
