7b143a19782482acc03e0a5dc30f546c4b581156
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-07-15 11:25:07 +0800

    rest: add static paths.

--- apps/rest-apis/src/app.mjs
+++ apps/rest-apis/src/app.mjs
@@ -1,5 +1,11 @@
+import path from "node:path"
 import express from "express"
+
 import routes from "./routes/index.mjs"
+import { fileURLToPath } from "node:url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default function createApp() {
   const app = express()
@@ -7,6 +13,7 @@ export default function createApp() {
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
 
+  app.use("/public", express.static(path.join(__dirname, "../public")))
   app.use("/api", routes)
 
   return app
