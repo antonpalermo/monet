ea256c76afa9c846cd2e410dec53aee23c31fe34
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-07-23 20:39:45 +0800

    rest: use redis session store.

--- apps/rest-apis/src/app.mjs
+++ apps/rest-apis/src/app.mjs
@@ -6,15 +6,35 @@ import passport from "passport"
 import session from "express-session"
 import cookieParser from "cookie-parser"
 
+import { RedisStore } from "connect-redis"
+import { createClient } from "redis"
+
 import routes from "./routes/index.mjs"
 
 import "./strategies/google-strategy.mjs"
+
+import debug from "debug"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default function createApp() {
+  const redisLogger = debug("rest:redis")
+
   const app = express()
+  const redis = createClient({
+    url: process.env.REDIS_URL
+  })
+
+  redis
+    .connect()
+    .then(() => redisLogger("connected"))
+    .catch(() => redisLogger("unable to connect to redis"))
+
+  const redisStore = new RedisStore({
+    client: redis,
+    prefix: "auth:"
+  })
 
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
@@ -29,7 +49,8 @@ export default function createApp() {
       secret: process.env.COOKIE_SECRET,
       cookie: {
         maxAge: 1000 * 60 * 60 * 24
-      }
+      },
+      store: redisStore
     })
   )
 
