e73a92d37c310ccb64ecb2f82bf6c74360a2dc0b
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-07-31 19:04:40 +0800

    client: init react-query

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+
 import { RouterProvider } from "react-router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import "./index.css"
 import routes from "./routes"
@@ -8,12 +10,16 @@ import routes from "./routes"
 import { LedgerProvider } from "@/contexts/ledger"
 import { SessionProvider } from "@/contexts/session"
 
+const client = new QueryClient()
+
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <SessionProvider>
-      <LedgerProvider>
-        <RouterProvider router={routes} />
-      </LedgerProvider>
-    </SessionProvider>
+    <QueryClientProvider client={client}>
+      <SessionProvider>
+        <LedgerProvider>
+          <RouterProvider router={routes} />
+        </LedgerProvider>
+      </SessionProvider>
+    </QueryClientProvider>
   </StrictMode>
 )
