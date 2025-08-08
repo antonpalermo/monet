7b89452ad29090a54585fdc87c5aab7f8821a12d
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-08-08 23:14:44 +0800

    client: revamp client signin

---
+++ apps/client/src/components/auth/sign-in.tsx
@@ -0,0 +1,65 @@
+import type { ComponentProps } from "react"
+import { Link } from "react-router"
+
+import { cn } from "@/lib/utils"
+
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
+export function SignIn({ className, ...props }: ComponentProps<"form">) {
+  return (
+    <>
+      <form className={cn("flex flex-col gap-6", className)} {...props}>
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h1 className="text-2xl font-bold">Login to your account</h1>
+          <p className="text-muted-foreground text-sm text-balance">
+            Enter your email below to login to your account
+          </p>
+        </div>
+        <div className="grid gap-6">
+          <div className="grid gap-3">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com" />
+          </div>
+          <div className="grid gap-3">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <a
+                href="#"
+                className="ml-auto text-sm underline-offset-4 hover:underline"
+              >
+                Forgot your password?
+              </a>
+            </div>
+            <Input id="password" type="password" />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </div>
+      </form>
+      <div className="grid gap-6">
+        <div className="after:border-border mt-6 relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+          <span className="bg-background text-muted-foreground relative z-10 px-2">
+            Or continue with
+          </span>
+        </div>
+        <Link to={`${import.meta.env.VITE_BACKEND_URL}/api/auth/signin/google`}>
+          <Button variant="outline" className="w-full">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+              <path d="M 25.996094 48 C 13.3125 48 2.992188 37.683594 2.992188 25 C 2.992188 12.316406 13.3125 2 25.996094 2 C 31.742188 2 37.242188 4.128906 41.488281 7.996094 L 42.261719 8.703125 L 34.675781 16.289063 L 33.972656 15.6875 C 31.746094 13.78125 28.914063 12.730469 25.996094 12.730469 C 19.230469 12.730469 13.722656 18.234375 13.722656 25 C 13.722656 31.765625 19.230469 37.269531 25.996094 37.269531 C 30.875 37.269531 34.730469 34.777344 36.546875 30.53125 L 24.996094 30.53125 L 24.996094 20.175781 L 47.546875 20.207031 L 47.714844 21 C 48.890625 26.582031 47.949219 34.792969 43.183594 40.667969 C 39.238281 45.53125 33.457031 48 25.996094 48 Z"></path>
+            </svg>
+            Login with Google
+          </Button>
+        </Link>
+        <div className="text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <a href="#" className="underline underline-offset-4">
+            Sign up
+          </a>
+        </div>
+      </div>
+    </>
+  )
+}
--- apps/client/src/routes.tsx
+++ apps/client/src/routes.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router"
 
-import App from "./components/app"
-import SignIn from "./pages/signin"
-
 import { Transactions } from "@/components/transactions"
 
-import { AuthLayout } from "@/components/layouts/auth"
+import { AuthLayout } from "@/components/auth"
 import { DashboardLayout } from "@/components/layouts/dashboard"
+
+import App from "./components/app"
+import { SignIn } from "@/components/auth/sign-in"
 
 const routes = createBrowserRouter([
   {
