c243655430da8adb85b31d06e1f22fe21d4304f2
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-08-22 23:08:46 +0800

    rest: init new express app

---
+++ apps/core/app.mjs
@@ -0,0 +1,25 @@
+import path from "node:path"
+import url from "node:url"
+
+import express from "express"
+import cookieParser from "cookie-parser"
+import logger from "morgan"
+
+import indexRouter from "./routes/index.mjs"
+import usersRouter from "./routes/users.mjs"
+
+const app = express()
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+
+app.use("/", indexRouter)
+app.use("/users", usersRouter)
+
+export default app
