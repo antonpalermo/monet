49e7e3e22b464216fc429b5dd683c51e5c7d668a
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-08-31 21:08:50 +0800

    rest: update transaction schema

--- apps/core/models/transactions.mjs
+++ apps/core/models/transactions.mjs
@@ -4,6 +4,12 @@ const schema = new mongoose.Schema(
   {
     name: {
       type: mongoose.Schema.Types.String
+    },
+    amount: {
+      type: mongoose.Schema.Types.Number
+    },
+    type: {
+      type: mongoose.Schema.Types.String
     }
   },
   {
