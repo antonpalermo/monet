be7f1e9ac6819371b8d26873e6f06832d68ad133
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-09-20 00:09:06 +0800

    create transaction route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,35 @@
+import { neon } from "@neondatabase/serverless"
+import { drizzle } from "drizzle-orm/neon-http"
 import { Hono } from "hono"
+import { transactions } from "../database/schemas/transactions"
+import { HTTPException } from "hono/http-exception"
 
 const route = new Hono<{ Bindings: CloudflareBindings }>()
 
-route.post("/create", c => {
-  return c.text("ok")
+route.post("/create", async c => {
+  const body = await c.req.json()
+  const pgsql = neon(c.env.DATABASE_URL)
+  const db = drizzle({ client: pgsql })
+
+  try {
+    const result = await db
+      .insert(transactions)
+      .values({
+        name: body.name,
+        amount: body.amount
+      })
+      .returning()
+
+    return new Response(
+      JSON.stringify({ data: result[0], msg: "successfully created" }),
+      { status: 201 }
+    )
+  } catch (error) {
+    throw new HTTPException(500, {
+      message: "unable to create new transaction",
+      cause: error
+    })
+  }
 })
 
 export default route
