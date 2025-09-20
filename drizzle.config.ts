7e7a8bfdf55b2fac2c387f1ee2365f44996e564c
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-09-20 20:38:46 +0800

    refac: update drizzle schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,7 +10,7 @@ dotenv.config({
 
 export default defineConfig({
   out: "./migrations",
-  schema: "./src/database/schemas",
+  schema: "./server/database/schemas",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!
